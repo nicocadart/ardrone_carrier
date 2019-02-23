2001bc7b1fbc6d4cffa65d7bd927052b75176dbb
Cadart Nicolas <nicolas.cadart@ensta-paristech.fr>
2019-02-23 19:03:58 +0100

    [~] Fix bottomcam_converter : Use tf2_ros instead of tf, remove multiple publisher definition, add image_raw correction, send modified msgs to new virtual camera '/ardrone/bottom_new/'

---
+++ src/bottomcam_converter_node.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import CameraInfo, Image
+
+
+def cam_info_cb(msg):
+    msg.header.frame_id = 'ardrone_base_bottomcam_new'
+    bottomcam_info_pub.publish(msg)
+
+
+def image_cb(msg):
+    msg.header.frame_id = 'ardrone_base_bottomcam_new'
+    bottomcam_img_pub.publish(msg)
+
+
+if __name__ == '__main__':
+    rospy.init_node('bottomcam_correcter')
+
+    # init publishers and subscribers
+    bottomcam_info_sub = rospy.Subscriber('/ardrone/bottom/camera_info', CameraInfo, cam_info_cb, queue_size=10)
+    bottomcam_info_pub = rospy.Publisher('/ardrone/bottom_new/camera_info', CameraInfo, queue_size=10)
+
+    bottomcam_img_sub = rospy.Subscriber('/ardrone/bottom/image_raw', Image, image_cb, queue_size=10)
+    bottomcam_img_pub = rospy.Publisher('/ardrone/bottom_new/image_raw', Image, queue_size=10)
+
+    rospy.loginfo("Bottom cam converter node ready.")
+    rospy.spin()
+    rospy.logwarn("Bottom cam converter node shutdown.")
